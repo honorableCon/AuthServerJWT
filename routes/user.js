00d04ab7c0ffd387a8c87902d2bd5a2fdd9a82d9
Honorable con <honorablecon@gmail.com>
2022-07-10 01:36:12 +0000

    update route

--- routes/user.js
+++ routes/user.js
@@ -19,11 +19,10 @@ router.post('/login', async (req, res) => {
         const { email, role, status } = user;
         const payload = { email, role, status };
         const token = await createToken(payload);
-        res.header('Authorization', `Bearer ${token}`);
-        return res.status(200).send(payload);
+        return res.status(200).send({ token });
     }
 
-    return res.status(400).send('Invalid email or password');
+    return res.status(400).json({error:'Invalid email or password'});
 })
 
 module.exports = router;
